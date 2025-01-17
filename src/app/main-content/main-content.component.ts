ba0a617db7c3bf36edb97042dc7328c4bd0a58c2
Joancs DEV <126966348+JoanCasanovasSalvatella@users.noreply.github.com>
2025-01-17 19:56:41 +0100

    Add files via upload

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -13,7 +13,7 @@ export class MainContentComponent {
   //Carroussel automatico de los productos
   productos = [
     {id:1, nombre: 'Nvidia RTX 4090', imagen: 'assets/prod1.png', desc: 'Tarjeta grafica', precio: '210.99€', precioAnterior: '210.99€'},
-    {id:2, nombre: 'Asus Prime B550-Plus', imagen: 'assets/prod2.jpg', desc: 'Placa base', precio: '129.99€', precioAnterior: '129.99€'},
+    {id:2, nombre: 'Asus Prime B550', imagen: 'assets/prod2.jpg', desc: 'Placa base', precio: '129.99€', precioAnterior: '129.99€'},
     {id:3, nombre: 'Sofá de 4 plazas', imagen: 'assets/prod3.png', desc: 'Sofá de 4 plazas', precio: '799€', precioAnterior: '799€'},
     {id:4, nombre: 'Lavabora', imagen: 'assets/prod4.png', desc: 'Lavadora bosch', precio: '820€', precioAnterior: '820€'}
   ]
@@ -51,6 +51,7 @@ export class MainContentComponent {
       // Cambiar el estado de isDark para la próxima llamada
       body.setAttribute("data-is-dark", String(!isDark));
     }
+
 }
 
 
