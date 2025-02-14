15309f8a620fdbb91e2234fffb2e8e93e8016a45
aroqiler <156545745+aroqiler@users.noreply.github.com>
2025-02-14 17:59:16 +0100

    a

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  login: FormGroup;
+  correo: FormControl;
+  contrasena: FormControl;
+  constructor() {
+    this.correo = new FormControl('', [Validators.required]);
+    this.contrasena = new FormControl('', [Validators.required]);
 
+    this.login = new FormGroup({
+      correo: this.correo,
+      contrasena: this.contrasena,
+    });
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registro: FormGroup;
+  nombre: FormControl;
+  correo: FormControl;
+  contrasena: FormControl;
+  repetirContrasena: FormControl;
 
+  constructor() {
+    this.nombre = new FormControl('', [Validators.required]);
+    this.correo = new FormControl('', [Validators.required, Validators.email]);
+    this.contrasena = new FormControl('', [Validators.required]);
+    this.repetirContrasena = new FormControl('', [Validators.required]);
+
+    this.registro = new FormGroup({
+      nombre: this.nombre,
+      correo: this.correo,
+      contrasena: this.contrasena,
+      repetirContrasena: this.repetirContrasena
+    });
+  }
 }
