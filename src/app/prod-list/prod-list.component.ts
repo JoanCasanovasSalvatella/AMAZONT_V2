5c673dda8318752dfcf77dd4d5006424dbb01541
aroqiler <156545745+aroqiler@users.noreply.github.com>
2025-02-21 17:42:57 +0100

    Y A

--- src/app/prod-list/prod-list.component.ts
+++ src/app/prod-list/prod-list.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-prod-list',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './prod-list.component.html',
   styleUrl: './prod-list.component.css'
 })
