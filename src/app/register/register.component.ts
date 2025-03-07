c28e50be3b94fc79bd64e8936bf3a49d75628c36
Joancs DEV <126966348+JoanCasanovasSalvatella@users.noreply.github.com>
2025-03-07 16:07:02 +0100

    Rol agregado al registro

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -15,18 +15,23 @@ export class RegisterComponent {
   correo: FormControl;
   contrasena: FormControl;
   repetirContrasena: FormControl;
+  rol: FormControl;
 
   constructor() {
+    //Nos aseguramos que el usuario tenga que introducir datos
     this.nombre = new FormControl('', [Validators.required]);
     this.correo = new FormControl('', [Validators.required, Validators.email]);
     this.contrasena = new FormControl('', [Validators.required]);
     this.repetirContrasena = new FormControl('', [Validators.required]);
+    this.rol = new FormControl('', [Validators.required])
 
+    //Establecemos los valores por defecto
     this.registro = new FormGroup({
       nombre: this.nombre,
       correo: this.correo,
       contrasena: this.contrasena,
-      repetirContrasena: this.repetirContrasena
+      repetirContrasena: this.repetirContrasena,
+      rol: this.rol
     });
   }
 }
