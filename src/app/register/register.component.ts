66b37f8ee6bb9e5eb2329adb0f944ffa468057e5
aroqiler <156545745+aroqiler@users.noreply.github.com>
2025-03-12 16:50:54 +0100

    Update register.component.ts ROL AÑADIDO

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -18,14 +18,12 @@ export class RegisterComponent {
   rol: FormControl;
 
   constructor() {
-    //Nos aseguramos que el usuario tenga que introducir datos
     this.nombre = new FormControl('', [Validators.required]);
     this.correo = new FormControl('', [Validators.required, Validators.email]);
     this.contrasena = new FormControl('', [Validators.required]);
     this.repetirContrasena = new FormControl('', [Validators.required]);
     this.rol = new FormControl('', [Validators.required])
 
-    //Establecemos los valores por defecto
     this.registro = new FormGroup({
       nombre: this.nombre,
       correo: this.correo,
