3b5db1556dc98ed032174132dfaeb0d31c598039
Joancs DEV <126966348+JoanCasanovasSalvatella@users.noreply.github.com>
2025-03-17 16:48:49 +0100

    componente recover-pwd

---
+++ src/app/recover-pwd/recover-pwd.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recover-pwd',
+  standalone: true,
+  imports: [],
+  templateUrl: './recover-pwd.component.html',
+  styleUrl: './recover-pwd.component.css'
+})
+export class RecoverPwdComponent {
+
+}
