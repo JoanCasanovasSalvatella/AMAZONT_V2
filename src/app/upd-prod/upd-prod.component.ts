83c0df3793275e171ccd4ae69895463add473da7
Joancs DEV <126966348+JoanCasanovasSalvatella@users.noreply.github.com>
2025-04-01 17:53:00 +0200

    UPD PRODUCTS Y HISTORIAL DE COMPRAS

---
+++ src/app/upd-prod/upd-prod.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upd-prod',
+  standalone: true,
+  imports: [],
+  templateUrl: './upd-prod.component.html',
+  styleUrl: './upd-prod.component.css'
+})
+export class UpdProdComponent {
+
+}
