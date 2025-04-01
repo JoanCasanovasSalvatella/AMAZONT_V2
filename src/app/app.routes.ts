48d26253fb3f790e503a11d6ca9370b6e9a8e9db
Joancs DEV <126966348+JoanCasanovasSalvatella@users.noreply.github.com>
2025-04-01 17:59:49 +0200

    RUTAS TPM AWEONAO

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,16 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProdDetailsComponent } from './prod-details/prod-details.component';
 import { RecoverPwdComponent } from './recover-pwd/recover-pwd.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { ProcessoPagoComponent } from './processo-pago/processo-pago.component';
+import { SendComponent } from './send/send.component';
+import { AddCardComponent } from './add-card/add-card.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UserProductsComponent } from './user-products/user-products.component';
+import { RecordComponent } from './record/record.component';
+import { PayMethodsComponent } from './pay-methods/pay-methods.component';
+import { AddProdComponent } from './add-prod/add-prod.component';
+import { UpdProdComponent } from './upd-prod/upd-prod.component';
 
 export const routes: Routes = [
   {
@@ -48,5 +58,60 @@ export const routes: Routes = [
     path: 'recoverPassword',
     title: 'Recover Password',
     component: RecoverPwdComponent
+  },
+  {
+    path: 'shopping_cart',
+    title: 'SHOPPING CART',
+    component: CarritoComponent,
+    children:[
+      {
+        path: 'payment',
+        title: 'Payment',
+        component: ProcessoPagoComponent
+      },
+      {
+        path: 'confirm',
+        title: 'Delivery',
+        component: SendComponent
+      },
+      {
+        path: 'addCard',
+        title: 'Add card',
+        component: AddCardComponent
+      }
+    ]
+  },
+  {
+    path: 'profile',
+    title: 'Profile',
+    component: ProfileComponent,
+    children:[
+      {
+        path: 'data',
+        title: 'User Data',
+        component: UserProductsComponent
+      },
+      {
+        path: 'user_record',
+        title: 'User record',
+        component: RecordComponent
+      },
+      {
+        path: 'user_pay_method',
+        title: 'User payment methods',
+        component: PayMethodsComponent
+      }
+    ]
+  },
+  {
+    path: 'add_product',
+    title: 'Add Product',
+    component: AddProdComponent
+  },
+  {
+    path: 'update_product',
+    title: 'Update Product',
+    component: UpdProdComponent
   }
+
 ];
