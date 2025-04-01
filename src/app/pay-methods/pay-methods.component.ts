ecb25d0281aad1963b0202f02f37e66ff8ecea82
Joancs DEV <126966348+JoanCasanovasSalvatella@users.noreply.github.com>
2025-04-01 17:49:41 +0200

    payMethods y processoPago

---
+++ src/app/pay-methods/pay-methods.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pay-methods',
+  standalone: true,
+  imports: [],
+  templateUrl: './pay-methods.component.html',
+  styleUrl: './pay-methods.component.css'
+})
+export class PayMethodsComponent {
+
+}
